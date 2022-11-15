9ad0d5f7b2c1115cf423b67957a8b121775b95c5
Sampath Kumar <sampathkumarvuradi@gmail.com>
2022-11-15 19:25:15 +0530

    Added Animations & Contact page

--- src/animation.js
+++ src/animation.js
@@ -79,3 +79,19 @@ export const sliderContainer = {
     },
   },
 };
+export const scrollRevel = {
+  hidden: {
+    opacity: 0,
+    scale: 1.2,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,10 +3,18 @@ import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollRevel } from "../animation";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <Faq>
+    <Faq
+      variants={scrollRevel}
+      ref={element}
+      animate={controls}
+      initial='hidden'
+    >
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
